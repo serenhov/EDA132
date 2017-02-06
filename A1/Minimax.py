ed155dac75705e252eb8d0d755eb05315d63a1e6
lisasilfversten <lisasilfversten@gmail.com>
2017-02-06 17:31:00 +0100

    minimax

---
+++ A1/Minimax.py
@@ -0,0 +1,19 @@
+UP, DOWN, LEFT, RIGHT, UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT = -10, 10, -1, 1, -11, -9, 9, 11
+DIRECTIONS = (UP, DOWN, LEFT, RIGHT, UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT)
+
+
+xlocations = []
+
+
+def checkDirections(node):
+    for d in DIRECTIONS:
+
+def findPossibleMoves(board, player):
+#or(x in xlocation):
+    for j in range(columns)
+        for i in range(rows)
+            if(board[i][j] == 'X')
+                checkDirections(board)
+
+
+    return findPossibleMoves(board, player)
--- A1/reversi.py
+++ A1/reversi.py
@@ -1,7 +1,5 @@
 from pip._vendor.distlib.compat import raw_input
 
-- 2 player
-- provide a leagal move
 
 EMPTY, BLACK, WHITE, OUTER = '-', 'X', 'O', '?'
 PIECES = (EMPTY, BLACK, WHITE, OUTER)
@@ -28,41 +26,7 @@ def printBoard(board):
 printBoard(board)
 
 
-def minmax(player, board, alpha, beta, depth, evaluate):
-    if depth == 0:
-        return evaluate(player, board), None
 
-    def value(board, alpha, beta):
-        return -minmax(opponent(player), board, -beta, -alpha, depth - 1, evaluate)[0]
-
-    moves = legal_moves(player, board)
-    if not moves:
-        if not any_legal_move(opponent(player), board):
-            return final_value(player, board), None
-        return value(board, alpha, beta), None
-    best_move = moves[0]
-    for move in moves:
-        if alpha >= beta:
-            break
-        val = value(make_move(move, player, list(board)), alpha, beta)
-        if val > alpha:
-            alpha = val
-            best_move = move
-        return alpha, best_move
-
-def alphabeta_searcher(depth, evaluate):
-    def strategy(player, board):
-        return minmax(player, board, minVal, maxVal, depth, evaluate)[1]
-
-    return strategy
-
-
-
-
-
-
-def AImove(board )
-        return board
 
 
 
